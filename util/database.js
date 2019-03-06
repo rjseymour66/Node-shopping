3f08114dc044ea10bb318b7c5b76bc0f490fe895
RJS66 <rjs.github.com>
2019-03-06 18:42:21 -0500

    Sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-  host: process.env.SQL_HOST,
-  user: process.env.SQL_USER,
-  database: process.env.SQL_DB,
-  password: process.env.SQL_PASS
+const sequelize = new Sequelize( process.env.SQL_DB, process.env.SQL_USER, process.env.SQL_PASS, {
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
