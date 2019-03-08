a54a0f0e32e8cf7b3937d3d1a6c6b1d23d18734d
RJS66 <rjs.github.com>
2019-03-07 22:50:15 -0500

    Mongoose

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 const path = require('path');
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
 const app = express();
@@ -16,16 +16,16 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use((req, res, next) => {
-  User.findById('5c809fe076ddc24d57677a34')
-  .then(user => {
-    req.user = new User(user.name, user.email, user.cart, user._id);
-    next();
-  })
-  .catch(err => console.log(err))
+  User.findById('5c81d6785c59b05cf8994483')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err))
 })
 
 app.use('/admin', adminRoutes)
@@ -33,6 +33,24 @@ app.use(shopRoutes)
 
 app.use(errorController.get404)
 
-mongoConnect(() => {
-  app.listen(4000)
-});
+mongoose.connect(`mongodb+srv://shopper1:${process.env.MONGODB_PASS}@cluster0-egia9.mongodb.net/shop?retryWrites=true1`)
+
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Ryan',
+          email: 'ryan@test.com',
+          cart: {
+            items: []
+          }
+        })
+        user.save()
+      }
+    })
+
+    app.listen(4000)
+  })
+  .catch(err => {
+    console.log(err);
+  })
--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,4 @@
-const mongodb = require('mongodb')
 const Product = require('../models/product')
-
-const ObjectId = mongodb.ObjectId;
 
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
@@ -16,13 +13,13 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(
-    title, 
-    price, 
-    description, 
-    imageUrl, 
-    null, 
-    req.user._id);
+  const product = new Product({
+    title: title,
+    price: price, 
+    description: description, 
+    imageUrl: imageUrl,
+    userId: req.user
+  });
   product
     .save()
     .then(result => {
@@ -59,14 +56,15 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-    const product = new Product(
-      updatedTitle, 
-      updatedPrice, 
-      updatedDesc,
-      updatedImageUrl, 
-      new ObjectId(prodId))
-    product
-    .save()
+
+  Product.findById(prodId)
+  .then(product => {
+    product.title = updatedTitle;
+    product.price = updatedPrice;
+    product.imageUrl = updatedImageUrl;
+    product.description = updatedDesc;
+    return product.save()
+  })
     .then(result => {
       console.log('**Updated product**');
       res.redirect('/admin/products')
@@ -77,8 +75,11 @@ exports.postEditProduct = (req, res, next) => {
 }
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
+  // .select('title price -_id')  **THIS IS IMPORTANT**
+  // .populate('userId', 'name')  **THIS IS IMPORTANT**
     .then(products => {
+      console.log(products);
       res.render('admin/products', {
         prods: products,
         pageTitle: 'Admin Products',
@@ -90,8 +91,8 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId)
-    .then(result => {
+  Product.findByIdAndRemove(prodId)
+    .then(() => {
       console.log('**Deleted product**');
       res.redirect('/admin/products')
     })
