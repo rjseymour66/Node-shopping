83ab70c404e7b86e638b5e749932cafb8c48b741
RJS66 <rjs.github.com>
2019-03-08 17:38:07 -0500

    Cookies

--- app.js
+++ app.js
@@ -4,36 +4,54 @@ const path = require('path');
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const session = require('express-session')
+const MongoDBStore = require('connect-mongodb-session')(session)
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const app = express();
+const store = new MongoDBStore({
+  uri: process.env.MONGODB_URI,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
+const authRoutes = require('./routes/auth')
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(session({
+  secret: process.env.COOKIE_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  store: store
+}))
 
 app.use((req, res, next) => {
-  User.findById('5c81d6785c59b05cf8994483')
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then(user => {
       req.user = user;
       next();
     })
-    .catch(err => console.log(err))
-})
+    .catch(err => console.log(err));
+});
+
 
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
+app.use(authRoutes)
 
 app.use(errorController.get404)
 
-mongoose.connect(`mongodb+srv://shopper1:${process.env.MONGODB_PASS}@cluster0-egia9.mongodb.net/shop?retryWrites=true1`)
+mongoose.connect(process.env.MONGODB_URI)
 
   .then(result => {
     User.findOne().then(user => {
