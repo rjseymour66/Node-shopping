7ee0361ef03d37f69e40ca3a1601f18f39888a11
RJS66 <rjs.github.com>
2019-03-08 22:07:51 -0500

    Sending Emails

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,14 @@
 const bcrypt = require('bcryptjs')
+const nodemailer = require('nodemailer')
+const sendgridTransport = require('nodemailer-sendgrid-transport')
+
 const User = require('../models/user');
+
+const transporter = nodemailer.createTransport(sendgridTransport({
+  auth: {
+    api_key: process.env.SENDGRID_API_KEY
+  }
+}))
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash('error');
@@ -81,9 +90,20 @@ exports.postSignup = (req, res, next) => {
         })
         .then(result => {
           res.redirect('/login')
-        });
+          transporter.sendMail({
+            to: email,
+            from: 'shop@node-complete.com',
+            subject: 'Signup successful!',
+            html: '<h1>You successfully signed up!</h1>'
+          })
+        })
+        .catch(err => {
+          console.log(err);
+        })
     })
-    .catch(err => console.log(err))
+    .catch(err => {
+      console.log(err);
+    })
 };
 
 exports.postLogout = (req, res, next) => {
