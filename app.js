e2676d6cbceea402b9ae2da8ef53d95aed5e191b
RJS66 <rjs.github.com>
2019-03-08 21:40:43 -0500

    Authentication 1

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const session = require('express-session')
 const MongoDBStore = require('connect-mongodb-session')(session)
+const csrf = require('csurf')
+const flash = require('connect-flash')
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -15,6 +17,7 @@ const store = new MongoDBStore({
   uri: process.env.MONGODB_URI,
   collection: 'sessions'
 });
+const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -30,7 +33,11 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   store: store
-}))
+  })
+)
+
+app.use(csrfProtection);
+app.use(flash());
 
 app.use((req, res, next) => {
   if (!req.session.user) {
@@ -44,6 +51,11 @@ app.use((req, res, next) => {
     .catch(err => console.log(err));
 });
 
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+})
 
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
@@ -52,21 +64,7 @@ app.use(authRoutes)
 app.use(errorController.get404)
 
 mongoose.connect(process.env.MONGODB_URI)
-
   .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'Ryan',
-          email: 'ryan@test.com',
-          cart: {
-            items: []
-          }
-        })
-        user.save()
-      }
-    })
-
     app.listen(4000)
   })
   .catch(err => {
